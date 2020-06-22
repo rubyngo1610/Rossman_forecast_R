03a96af3e619570d524ed8de1d3bd52560ecdd7c
nhihuynh107 <67246821+nhihuynh107@users.noreply.github.com>
2020-06-21 20:36:31 -0400

    Add files via upload

---
+++ DataCleaning_train.R
@@ -0,0 +1,15 @@
+#Import data
+train <- read.csv("train.csv")
+
+#Check if there's any NA
+table (complete.cases (train))
+#no NULL values found
+
+summary(train)
+#Sales and Customers have outliers - Max values are much higher than the mean
+boxplot.default(train$Customers,horizontal = TRUE)
+#Verified: Customers have outliers
+boxplot.default(train$Sales,horizontal = TRUE)
+#Verified: Sales have outliers
+
+head(train,n=5)
