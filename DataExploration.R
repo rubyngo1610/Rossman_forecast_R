fad995c1289a14d03e05e076521ab62763cbc08b
nhihuynh107 <67246821+nhihuynh107@users.noreply.github.com>
2020-07-11 20:35:28 -0400

    Update DataExploration.R

--- DataExploration.R
+++ DataExploration.R
@@ -1,4 +1,4 @@
-mergedata <- read.csv("merge.csv")
+#EXPLORATION
 options("scipen" = 10) #for graph to show full number
 library(ggplot2)
 
@@ -6,59 +6,62 @@ library(ggplot2)
 #Min,Max of total sales
 AA <- vector(mode = "numeric",length = 1115)
 for (i in 1:1115) {
-  AA[i] <- sum(mergedata$Sales[mergedata$Store==i])
+  AA[i] <- sum(train_store$Sales[train_store$Store==i])
 }
 match(max(AA),AA) #--> Store 262 has the max total sales
 match(min(AA),AA) #--> Store 307 has the min total sales
-#Look at total sales for the first 100 stores
-A <- vector(mode = "numeric",length = 100)
-for (i in 1:100) {
-  A[i] <- sum(mergedata$Sales[mergedata$Store==i])
+#Number of stores in each range of sales
+A <- vector(mode = "numeric",length = 1115)
+for (i in 1:1115) {
+  A[i] <- mean(train_store$Sales[train_store$Store==i])
 }
-a <- as.character(as.numeric(1:100))
-barplot(A,main="Total sales by Store",names.arg = dput(a),xlab="Store")
+summary(A) #get value of 1st quadrant and 3rd quadrant
+firstquantile <- A[A<=4412] 
+middle <- A[A>4412 & A<6634]
+thirdquantile <- A[A>=6634] 
+barplot(c(length(firstquantile),length(middle),length(thirdquantile))
+,main="Number of stores by sales range",names.arg = c("1st quantile",
+"Middle","3rd quantile"),xlab="Sales",ylab="No. of stores")
 #--> Sales vary from stores to stores --> Strong predictor value
 
 ##Sales ~ DayOfWeek
-mergedata$DayOfWeek <- as.factor(as.integer(mergedata$DayOfWeek))
-B <- vector(mode = "numeric",length = 7)
-for (i in 1:7) {
-  B[i] <- sum(mergedata$Sales[mergedata$DayOfWeek==i])
-}
-b <- as.character(as.numeric(1:7))
-barplot(B,main="Total sales by Day of Week",names.arg = dput(b))
+ggplot(train_store, aes(x=Date,y=Sales,color=DayOfWeek)) + geom_smooth()
 #--> Day 7 is extremely low compared to other dates --> Strong predictor value
+#Look closer at Day 7
+train$DayOfWeek <- as.integer(as.factor(train$DayOfWeek))
+Day7Sales <- subset(train_store,DayOfWeek==7)
+summary(Day7Sales) #-> Many stores have 0 sales on Sunday
+Day7Sales$Open <- as.integer(as.factor(Day7Sales$Open))
+nrow(subset(Day7Sales,Open==0)) #--> All stores are open on Day 7,
+#but some of them don't have any sales and customers at all
+nrow(Day7Sales) - nrow(subset(Day7Sales,Sales==0))
+#--> 3593/144730 records have sales on Day 7
 
 ##Sales ~ Date
-Day1 <- subset(mergedata,Date == "2015-07-31",select = c(Store,Sales))
-E <- vector(mode = "numeric",length = 50)
-for (i in 1:50) {
-  E[i] <- Day1$Sales[Day1$Store == i]
-}
-e <- as.character(as.numeric(1:50))
-barplot(E,main="Sales each store on 7/31/2015",names.arg = dput(e))
-#--> Sales on the same date varies significantly --> Check for correlations
-# with StoreType, Assortments, CompetitorDistance, Promo, Promo2, PromoInterval
+ggplot(train_store, aes(x=Date,y=Sales)) + geom_smooth()
+#-->Sales increased from 2013 to 2015
 
 ##Sales ~ Customers
 C <- vector(mode = "numeric",length = 7)
 for (i in 1:7) {
-  C[i] <- sum(mergedata$Customers[mergedata$DayOfWeek==i])
+  C[i] <- sum(train_store$Customers[train_store$DayOfWeek==i])
 }
 barplot(C,main="Total customers by Day of Week",names.arg = dput(b))
 #--> Day 1 the highest volume, 
 # Check for positive correlation
-D <- matrix(1:StoreTotal,nrow=StoreTotal,ncol=1)
-for (i in 1:StoreTotal) {
-  D[i] <- sum(mergedata$Customers[mergedata$Store==i])
+D <- matrix(1:1115,nrow=1115,ncol=1)
+for (i in 1:1115) {
+  D[i] <- sum(train_store$Customers[train_store$Store==i])
 }
-qplot(A,D) + xlab("Sales")+ ylab("Customers") +geom_smooth(method="lm")
+qplot(AA,D) + xlab("Sales") + ylab("Customers") + geom_smooth(method="lm")
 #--> Store and Customers are positively related
 
 ##Sales ~ Open
+#Some stores were opened but didn't have any sales
+
 ##Sales ~ StateHoliday
-OpenOnHoliday <- subset(mergedata,StateHoliday != 0 & Open==1)
-NotOpenOnHoliday <- subset(mergedata,StateHoliday != 0 & Open==0)
+OpenOnHoliday <- subset(train_store,StateHoliday != 0 & Open==1)
+NotOpenOnHoliday <- subset(train_store,StateHoliday != 0 & Open==0)
 nrow(OpenOnHoliday) 
 nrow(NotOpenOnHoliday) #--> 910/31320 records open on holidays
 a1 <- sum(OpenOnHoliday$Sales[OpenOnHoliday$StateHoliday=="a"])
@@ -69,21 +72,27 @@ barplot(c(a1,b1,c1),main="Total sales on each holiday",names.arg = c("a","b","c"
 # highest sale (5890305), significantly more than Easter (b) (1433744) and
 # Christmas (c) (691806)
 
+#Try to compare sales between dates with holiday and non-holiday --> Ask chi Phuong
+
 ##Sales ~ SchoolHoliday
-m <-  0
-for (n in 1:1115) {
-  sales1 <- mean(mergedata$Sales[mergedata$Store==n & mergedata$SchoolHoliday==1])
-  sales2 <-mean(mergedata$Sales[mergedata$Store==n & mergedata$SchoolHoliday==0])
-  if (abs(sales2-sales1) > 500) { m = m + 1 }
+m <- 0
+n <- 0
+for (j in 1:1115) {
+  sales1 <- mean(train_store$Sales[train_store$Store==j & train_store$SchoolHoliday==1])
+  sales2 <- mean(train_store$Sales[train_store$Store==j & train_store$SchoolHoliday==0])
+  if (sales2-sales1 > 0) { m = m + 1 }
+  else if (sales2-sales1 < 0) { n = n + 1 }
 }
 m
-#--> 866/1115 stores that was significantly affected by school holidays
+n
+#--> Sales was significantly lower on school holidays
 #--> Strong predictor value
 
 ##Sales ~ StoreType
-a2 <- sum(mergedata$Sales[mergedata$StoreType=="a"])
-b2 <- sum(mergedata$Sales[mergedata$StoreType=="b"])
-c2 <- sum(mergedata$Sales[mergedata$StoreType=="c"])
-d <- sum(mergedata$Sales[mergedata$StoreType=="d"])
-barplot(c(a2,b2,c2,d),main="Total sales of each store type",names.arg = c("a","b","c","d"))
-#--> Type a has the highest sales, with b is the lowest (a>d>c>b)
+a2 <- mean(train_store$Sales[train_store$StoreType=="a"])
+b2 <- mean(train_store$Sales[train_store$StoreType=="b"])
+c2 <- mean(train_store$Sales[train_store$StoreType=="c"])
+d2 <- mean(train_store$Sales[train_store$StoreType=="d"])
+barplot(c(a2,b2,c2,d2),main="Mean sales of each store type",names.arg = c("a","b","c","d"))
+
+#--> Type b has the highest sales
